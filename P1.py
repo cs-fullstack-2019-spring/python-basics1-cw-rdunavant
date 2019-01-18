3c7105f33c456a2263648f0f1e5eb3b4db53b70d
rdunavant0 <rdunavant0@gmail.com>
2019-01-18 11:48:17 -0600

    message

---
+++ P1.py
@@ -0,0 +1,32 @@
+def main():
+    # problem1()
+    # problem2()
+    # problem3()
+    # problem4()
+
+def problem1():
+    name1 = "myNameIs:"
+    name2 = "Rob"
+    print(name1 + name2);
+
+def problem2():
+    extraCredit = input("What is the extra credit you earned?")
+    if (extraCredit < 5)
+        print("That's not enough extra credit.")
+    if (extraCredit > 20)
+        print("That's too much extra credit.")
+
+def problem3():
+    password = input("Enter a password")
+    password2 = input("Reenter password")
+    if (password == password2)
+        print("Correct")
+
+def problem4():
+    cardNumber = input("Give me two card numbers")
+    if (cardNumber == 21)
+        print(BLACKJACK!)
+    if (cardNumber > 21)
+        print(BUST!)
+    if (cardNumber < 21)
+        print("The total is [THE TOTAL]")
